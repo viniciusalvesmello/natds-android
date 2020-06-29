9b69461fb43c1f14a47c50008b0a2b31268b250b
Marcella Souza <mlsouza@thoughtworks.com>
2020-06-29 12:06:44 -0300

    feat: 🎸 [DSY-1103]
    Adding custom dialog class

---
+++ designsystem/src/main/kotlin/com/natura/android/dialog/Dialog.kt
@@ -0,0 +1,10 @@
+package com.natura.android.dialog
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+
+class Dialog(context: Context) {
+
+    private val dialog = AlertDialog.Builder(context, com.natura.android.R.style.Theme_Natura)
+
+}
